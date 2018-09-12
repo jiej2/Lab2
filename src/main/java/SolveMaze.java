41939f86551b0a76a9ce035b875083181dc4af85
jijie <jiej2@illinois.edu>
2018-09-12 18:47:52 -0500

    lab2

--- src/main/java/SolveMaze.java
+++ src/main/java/SolveMaze.java
@@ -35,6 +35,24 @@ public class SolveMaze {
          */
         for (int step = 0; step < 1000; step++) {
             // Implement your maze solving algorithm here
+           while (!maze.isFinished()) {
+            if (maze.canMove()) {
+                maze.move();
+            } else {
+                maze.turnRight();
+            }
+            if (maze.canMove()) {
+                maze.move();
+            } else {
+                maze.turnLeft();
+            }
+            if (maze.canMove()) {
+                maze.move();
+            } else {
+                maze.turnRight();
+            }
+
+            }
         }
 
         if (maze.isFinished()) {
